bc38686c0cb4a4697ed34bddbaf7403df10c7542
Carlos-Eduardo-Bs <eduardo20power@gmail.com>
2026-05-18 22:28:10 -0300

    feat: implementada a instanciação da base e padrão DTO para usuários

--- src/main/java/com/spring/workshopmongo/resource/UserResource.java
+++ src/main/java/com/spring/workshopmongo/resource/UserResource.java
@@ -3,6 +3,7 @@ package com.spring.workshopmongo.resource;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.spring.workshopmongo.domain.User;
+import com.spring.workshopmongo.dto.UserDTO;
 import com.spring.workshopmongo.services.UserService;
 
 
@@ -21,8 +23,9 @@ public class UserResource {
     private UserService service; 
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
