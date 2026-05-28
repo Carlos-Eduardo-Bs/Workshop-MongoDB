6a201f623e341673f0bd35428301f01919c3c2ad
Carlos-Eduardo-Bs <eduardo20power@gmail.com>
2026-05-27 22:48:29 -0300

    feat: implementada consultas simples com query methods

--- src/main/java/com/spring/workshopmongo/resource/PostResource.java
+++ src/main/java/com/spring/workshopmongo/resource/PostResource.java
@@ -1,14 +1,18 @@
 package com.spring.workshopmongo.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import com.spring.workshopmongo.domain.Post;
+import com.spring.workshopmongo.resource.util.URL;
 import com.spring.workshopmongo.services.PostService;
 
 @RestController
@@ -22,5 +26,12 @@ public class PostResource {
     public ResponseEntity<Post> finById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+       @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> finByTitle(@RequestParam(value="text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
